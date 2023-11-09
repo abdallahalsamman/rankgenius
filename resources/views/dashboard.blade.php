9cdbd537579fe40e4886b088d064091d8c86e752
NazimNasser <nazimnasser94@gmail.com>
2023-11-09 09:27:40 +0200

    addd styles to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,33 +2,33 @@
 
 @section('content')
 <div class="flex">
-    <div class="w-1/4 bg-gray-100 h-screen">
-        <x-menu activate-by-route active-bg-color="bg-blue-50" class="border border-dashed">
+    <div class="min-w-[260px] bg-[#F7FAFC] h-screen mt-[30px]">
+        <div class="text-2xl text-center p-4">Nazim</div>
+        <x-menu activate-by-route active-bg-color="bg-[#EDF2F7]" class="border border-dashed">
+            <div class="flex flex-col justify-center gap-3 text-[17px]">
 
-            <x-menu-item title="Generate Articles" icon="o-plus" />
-            <x-menu-item title="Navigate to Alert docs" icon="o-arrow-right" link="/docs/components/alert" />
+                <div class="text-[12px] text-[#757575] pl-2 pt-6">ARTICLES</div>
+                <x-menu-item title="Generate Articles" icon="o-plus" />
+                <x-menu-item title="History" icon="o-clock" />
 
-            <x-menu-separator />
+                <div class="text-[12px] text-[#757575] pl-2 pt-6">CUSTOMIZATION</div>
+                <x-menu-item title="Presets" icon="s-square-3-stack-3d" />
+                <x-menu-item title="Brands" icon="m-globe-asia-australia" />
+                <x-menu-item title="Custom Images" icon="o-clock" />
 
-            <x-menu-sub title="Settings" icon="o-cog-6-tooth">
-                <x-menu-item title="Wifi" icon="o-wifi" />
-                <x-menu-item title="Archives" icon="o-archive-box" />
-            </x-menu-sub>
+                <x-menu-item title="Hello" badge="7" badge-classes="!badge-warning !text-red-500" />
 
-            <x-menu-separator title="Magic" icon="o-sparkles" />
-            <x-menu-item title="Hello" badge="7" badge-classes="!badge-warning !text-red-500" />
+                {{-- When route matches `link` property it activates menu --}}
+                <x-menu-item title="Active state" />
 
-            {{-- When route matches `link` property it activates menu --}}
-            <x-menu-item title="Active state" link="/docs/components/menu" />
+                <x-menu-item title="Hi" />
+                <x-menu-item title="Some style" class="text-purple-500 font-bold" />
 
-            <x-menu-separator title="Tricks" />
-            <x-menu-item title="Hi" />
-            <x-menu-item title="Some style" class="text-purple-500 font-bold" />
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <x-button label="Logout" icon="o-arrow-left-on-rectangle" type="submit" />
-            </form>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-button label="Logout" icon="o-arrow-left-on-rectangle" type="submit" />
+                </form>
+            </div>
         </x-menu>
     </div>
     <div class="w-3/4 bg-white h-screen">
