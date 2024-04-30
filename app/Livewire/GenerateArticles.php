c73d0d4b3f8a85c817d79f3a19a1feececa79f2c
Abdallah Alsamman <sammanabdallah@gmail.com>
2024-04-30 17:18:09 +0300

    update

--- app/Livewire/GenerateArticles.php
+++ app/Livewire/GenerateArticles.php
@@ -16,9 +16,15 @@ class GenerateArticles extends Component
     public $quantity = 1, $language = "English";
     public $preset = 0, $presetOptions = [];
     public $titles = "How to make bread at home?\nBest recipes for making bread\nWhat are the different types of bread";
+    public $keywords;
 
     public $simple_mode_allowed_article_quantity = [1, 3, 5, 10, 20];
     public $preset_mode_allowed_article_quantity = [1, 3, 5, 10, 20, 40, 60, 80, 100, 150, 300];
+
+    public function mount()
+    {
+        // $this->presetOptions = auth()->user()->presets->toArray();
+    }
 
     public function render()
     {
@@ -27,21 +33,12 @@ class GenerateArticles extends Component
 
     public function simpleMode()
     {
-        $this->validate([
-            'url' => "nullable|url",
-            'topic' => "required|min:30|max:1024",
-            'sitemap_url' => "nullable|url",
-            'quantity' => "required|integer|in:" . join(",", $this->simple_mode_allowed_article_quantity)
-        ]);
+        $this->validateInputs('simple_mode_allowed_article_quantity', BatchModeEnum::CONTEXT);
+    }
 
-        return $this->generateArticles(
-            BatchModeEnum::CONTEXT,
-            $this->url,
-            $this->topic,
-            $this->quantity,
-            $this->sitemap_url,
-            $this->external_linking
-        );
+    public function presetMode()
+    {
+        $this->validateInputs('preset_mode_allowed_article_quantity', Preset::find($this->preset)->mode, true);
     }
 
     public function titleMode()
@@ -50,10 +47,18 @@ class GenerateArticles extends Component
             'titles' => "required|min:10|max:1024",
         ]);
 
-        $mode = BatchModeEnum::TITLE;
-        $details = $this->titles;
+        // remove empty lines
+        $this->titles = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $this->titles);
         $quantity = substr_count($this->titles, "\n") + 1;
-        return $this->generateArticles($mode, null, $details, $quantity);
+        $this->generateArticles([
+            'mode' => BatchModeEnum::TITLE,
+            'url' => null,
+            'details' => $this->titles,
+            'quantity' => $quantity,
+            'language' => $this->language,
+            'external_linking' => $this->external_linking,
+            'sitemap_url' => $this->sitemap_url,
+        ]);
     }
 
     public function keywordMode()
@@ -62,46 +67,56 @@ class GenerateArticles extends Component
             'keywords' => "required|min:10|max:1024",
         ]);
 
-        $mode = BatchModeEnum::KEYWORD;
-        $details = $this->keywords;
-        $quantity = substr_count($this->titles, "\n") + 1;
-        return $this->generateArticles($mode, null, $details, $quantity);
+        // remove empty lines
+        $this->keywords = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $this->keywords);
+        $quantity = substr_count($this->keywords, "\n") + 1;
+        $this->generateArticles([
+            'mode' => BatchModeEnum::KEYWORD,
+            'url' => null,
+            'details' => $this->keywords,
+            'quantity' => $quantity,
+            'language' => $this->language,
+            'external_linking' => $this->external_linking,
+            'sitemap_url' => $this->sitemap_url,
+        ]);
     }
 
-    public function presetMode()
+    private function validateInputs($allowedQuantities, $mode, $isPreset = false)
     {
+        $details = $isPreset ? Preset::find($this->preset)->details : $this->topic;
         $this->validate([
-            'preset' => "required|exists:presets,id",
-            'quantity' => "required|integer|in:" . join(",", $this->preset_mode_allowed_article_quantity)
+            'url' => "nullable|url",
+            'topic' => "required|min:30|max:1024",
+            'sitemap_url' => "nullable|url",
+            'quantity' => "required|integer|in:" . join(",", $this->$allowedQuantities),
+            'preset' => $isPreset ? "required|exists:presets,id" : "nullable",
         ]);
 
-        $preset = Preset::where('id', $this->preset)->first();
-        $details = $preset->details;
-        $mode = $preset->mode;
-        $quantity = $this->quantity;
-        return $this->generateArticles($mode, null, $details, $quantity);
+        $this->generateArticles([
+            'mode' => $mode,
+            'url' => $this->url,
+            'details' => $details,
+            'quantity' => $this->quantity,
+            'language' => $this->language,
+            'external_linking' => $this->external_linking,
+            'sitemap_url' => $this->sitemap_url,
+        ]);
     }
 
-    public function mount()
-    {
-        $this->presetOptions = auth()->user()->presets->toArray();
-    }
-
-    public function generateArticles($mode, $url, $details, $quantity, $sitemap_url, $external_linking)
+    private function generateArticles($data)
     {
         $batch = Batch::create([
             'id' => Str::uuid(),
-            'mode' => $mode,
-            'url' => $url,
-            'details' => $details,
-            'language' => $this->language,
-            'quantity' => $quantity,
+            'mode' => $data['mode'],
+            'url' => $data['url'],
+            'details' => $data['details'],
+            'language' => $data['language'],
+            'quantity' => $data['quantity'],
             'status' => BatchStatusEnum::IN_PROGRESS,
             'user_id' => auth()->user()->id,
         ]);
 
         RunBatch::dispatch($batch);
-
         return redirect()->route('history.view', ['id' => $batch->id]);
     }
 }
