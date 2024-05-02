8f0336be8dd50816abbf40b95b31aa68939d92fd
abdallah alsamman <sammanabdallah@gmail.com>
2024-05-02 18:35:58 +0300

    update

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
-# ignoring platform reqs because of "alc/sitemap-crawler"
-just cmpsr install
-sudo chmod -R o+w storage/
-sudo chmod -R 775 storage/
-yarn dev &
-php artisan migrate
-php artisan optimize:clear
-while true; do
-	php artisan queue:work database --timeout=999999
-done;
+sudo chmod -R o+w storage/ &
+sudo chmod -R 775 storage/ &
+php artisan optimize:clear &
+php artisan migrate &
+yarn dev
