1e236a7a461888c2e0a5ef07ecc33626d4d21a85
Abdallah Alsamman <sammanabdallah@gmail.com>
2024-05-02 14:49:33 +0300

    update

---
+++ app/Console/Commands/InternalLinking.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\AIService;
+use Pgvector\Laravel\Distance;
+use Illuminate\Console\Command;
+use App\Models\SitemapEmbedding;
+
+class InternalLinking extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:internal-linking {query}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get relevant internal links for a given query embedding.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $query_embedding_text = $this->argument('query');
+        $query_embedding = AIService::generateEmbeddings([$query_embedding_text]);
+        dd($query_embedding);
+        echo SitemapEmbedding::query()->nearestNeighbors('embedding', $query_embedding[0]['text'], Distance::L2)->take(20)->get()->pluck('url');
+    }
+}
